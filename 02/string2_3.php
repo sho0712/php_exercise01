b35360e2fe5f084e8a633baa8db7b5d91527f05a
sho0712 <hqmhn857@yahoo.co.jp>
2022-02-25 23:35:08 +0900

    配列と連想配列の課題完了

---
+++ 02/string2_3.php
@@ -0,0 +1,11 @@
+<?php
+
+$msg = <<<END
+山へ、芝刈りに
+行きたくて
+うずうすしています。
+END;
+
+#var_dump($mag);
+
+echo $msg;
---
+++ 02/string2_4.php
@@ -0,0 +1,11 @@
+<?php
+
+$num = 9;
+#var_dump($num);
+$result = $num/2;
+
+#var_dump($result);
+
+$result++;
+
+echo $result;
